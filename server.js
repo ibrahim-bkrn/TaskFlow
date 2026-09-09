c44201381d1da8c3a5a13ebd47be71dc0528d5d1
ibrahim-bkrn <ibrahim.boukernafaa@gmail.com>
2026-09-09 12:12:40 +0200

    Ajout de la connexion à la bdd

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+const db = require('./Models/bdd.js')
+
+
+const PORT = process.env.PORT || 3300;
+app.listen(PORT, () => {
+    console.log(`Serveur en cours d'exécution sur http://localhost:${PORT}`);
+}); 
+
+app.get('/', (req, res) => {
+    res.send('Bienvenue sur notre projet')
+})
