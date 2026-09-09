8f2fb9c32d8c263dfaafd764e14d3b2c6c799120
Drif <yanis.drif@efrei.net>
2026-09-09 14:49:33 +0200

    Ajout du model et les controlleurs et routes pour les projets

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const projetController = require('./Controllers/projetController');
+
 const app = express();
 
 app.use(express.json());
-const db = require('./Models/bdd.js')
+const db = require('./Models/bdd')
 
 
 const PORT = process.env.PORT || 3300;
@@ -13,3 +15,9 @@ app.listen(PORT, () => {
 app.get('/', (req, res) => {
     res.send('Bienvenue sur notre projet')
 })
+
+app.get('/projets', projetController.getAllProjects);
+app.get('/projets/:id', projetController.getProjectsbyid);
+app.post('/projets', projetController.createProjets);
+app.put('/projets/:id', projetController.updateProjet);
+app.delete('/projets/:id', projetController.deleteProjets);
